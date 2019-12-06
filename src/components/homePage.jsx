ec9dee9212757b34d213c6dde1755439133c6c00
Preston Elliott <preston.elliott24@gmail.com>
2019-12-06 10:07:52 -0500

    moved app up to main level

---
+++ src/components/homePage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import {} from "react-bootstrap"
+import NavBar from "./navBar"
+
+export default function HomePage() {
+
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    )
+
+}
