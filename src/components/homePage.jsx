a87d256de4e37cb319d649f3ef0d13c374e92f27
Preston Elliott <preston.elliott24@gmail.com>
2019-12-11 17:47:08 -0500

    home page

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -4,11 +4,19 @@ import { Link } from 'react-router-dom'
 export default function HomePage() {
 
     return (
+        <>
         <div className="jumbotron">
             <Link to="about" className="btn btn-primary btn-lg">
                 About
             </Link>
         </div>
+
+        <div className="jumbotron">
+            <Link to="projects" className="btn btn-primary btn-lg">
+                Projects
+            </Link>
+        </div>
+        </>
     )
 
 }
